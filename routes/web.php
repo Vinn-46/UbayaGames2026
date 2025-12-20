60e60c8fc51ed4a01a128644e266267fbb93384e
Vinn <kelvinadrian168@gmail.com>
2025-12-20 23:46:31 +0700

    Aku nambahin bagian routes
    jadi aku nambahin untuk aboutus dan registrationnya yaaa

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// untuk about us
+Route::get('/aboutus', function () {
+    return view('aboutus');
+});
+
+// untuk registration
+Route::get('/registration', function () {
+    return view('registration');
+});
