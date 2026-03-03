ab6626174cc0f0e90e30aa023e0408d07de36b55
nawskana <166533685+nawskana@users.noreply.github.com>
2026-03-03 13:30:04 +0700

    update migrations

--- database/migrations/2026_02_07_070320_create_participants_table.php
+++ database/migrations/2026_02_07_070320_create_participants_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('participants', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('house_id')
+                  ->constrained('houses')
+                  ->cascadeOnDelete();
             $table->string('name', 255);
             $table->string('nrp',9);
             $table->string('major',45);
