f432b403b85ceb37b24ba1b35b72ae2096b86805
Vinn <kelvinadrian168@gmail.com>
2026-04-04 12:17:15 +0700

    Fix vite config path

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            // PENTING: Beritahu Vite kalau folder public-nya adalah root (titik)
+            publicDirectory: '.', 
         }),
     ],
+    // Tambahkan ini agar folder build tercipta di lokasi yang benar
+    build: {
+        outDir: 'build',
+    }
 });
